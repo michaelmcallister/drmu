312f12fbc22fd6354595b1ede46cc4119487d25b
Michael McAllister <me@michaelmcallister.org>
2017-01-16 22:04:55 +1100

    moved mux to config

--- drmu.go
+++ drmu.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/route53"
 	"github.com/spf13/viper"
@@ -13,25 +14,54 @@ import (
 )
 
 func updateHandler(w http.ResponseWriter, r *http.Request) {
-	name := pat.Param(r, "domainName")
+	domain_name := pat.Param(r, "domainName")
 	ip := pat.Param(r, "ipAddress")
-	fmt.Fprintf(w, "Updating %s to %s", name, ip)
+	fmt.Fprintf(w, "Updating %s to %s", domain_name, ip)
+
+	params := &route53.ChangeResourceRecordSetsInput{
+		ChangeBatch: &route53.ChangeBatch{
+			Changes: []*route53.Change{
+				{
+					Action: aws.String("UPSERT"),
+					ResourceRecordSet: &route53.ResourceRecordSet{
+						Name: aws.String(domain_name),
+						Type: aws.String("A"),
+						ResourceRecords: []*route53.ResourceRecord{
+							{
+								Value: aws.String(ip),
+							},
+						},
+						TTL: aws.Int64(300),
+					},
+				},
+			},
+			Comment: aws.String("Updated by DRMU"),
+		},
+		HostedZoneId: aws.String(""),
+	}
+	resp, err := client.ChangeResourceRecordSets(params)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Println(resp)
 }
 
 func main() {
-	client, err := initConfig()
-	if err != nil {
+	client, mux, err := initConfig()
+
+	if err != nil || client == nil || mux == nil {
 		fmt.Println("Failed to initialize configuration:", err)
 		return
 	}
 
-	mux := goji.NewMux()
 	mux.HandleFunc(pat.Get("/drmu/update/:domainName/:ipAddress"), updateHandler)
-
 	http.ListenAndServe("localhost:8000", mux)
 }
 
-func initConfig() (*route53.Route53, error) {
+func initConfig() (*route53.Route53, *goji.Mux, error) {
 	os.Setenv("AWS_SDK_LOAD_CONFIG", "1")
 
 	userConfig := viper.New()
@@ -41,22 +71,31 @@ func initConfig() (*route53.Route53, error) {
 	err := userConfig.ReadInConfig()
 
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	sess, err := session.NewSession()
 
 	if err != nil {
 		fmt.Println("Failed to create session:", err)
-		return nil, err
+		return nil, nil, err
 	}
 
 	client := route53.New(sess)
 
 	if err != nil {
 		fmt.Println("Failed to create client:", err)
-		return nil, err
+		return nil, nil, err
 	}
 
-	return client, nil
+	mux := goji.NewMux()
+
+	if mux == nil {
+		fmt.Println("Failed to instantiate HTTP routing engine:", err)
+		return nil, nil, err
+	} else {
+		//mux.HandleFunc(pat.Get("/drmu/update/:domainName/:ipAddress"), updateHandler)
+	}
+
+	return client, mux, nil
 }
