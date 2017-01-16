cbadf97b1484f5663b87440d215972db23f34581
c22 <c22@users.noreply.github.com>
2017-01-16 16:23:54 +1100

    Add goji handler

--- drmu.go
+++ drmu.go
@@ -1,30 +1,44 @@
 package main
 
 import (
-    "fmt"
-    "os"
+	"fmt"
+	"net/http"
+	"os"
 
-    "github.com/spf13/viper"
+	"github.com/spf13/viper"
+	"goji.io"
+	"goji.io/pat"
 )
 
-func main() {
-    err := initConfig()
-    if err != nil {
-        fmt.Println("Init error:", err)
-        return
-    }
+func updateHandler(w http.ResponseWriter, r *http.Request) {
+	name := pat.Param(r, "domainName")
+	ip := pat.Param(r, "ipAddress")
+	fmt.Fprintf(w, "Updating %s to %s", name, ip)
 }
 
-func initConfig() (error) {
-    os.Setenv("AWS_SDK_LOAD_CONFIG", "1")
+func main() {
+	err := initConfig()
+	if err != nil {
+		fmt.Println("Init error:", err)
+		return
+	}
 
-    userConfig := viper.New()
-    userConfig.SetConfigName("app")
-    userConfig.AddConfigPath(".")
-    userConfig.AddConfigPath("config")
-    err := userConfig.ReadInConfig()
-    if err != nil {
-        return err
-    }
-  return nil
+	mux := goji.NewMux()
+	mux.HandleFunc(pat.Get("/drmu/update/:domainName/:ipAddress"), updateHandler)
+
+	http.ListenAndServe("localhost:8000", mux)
+}
+
+func initConfig() error {
+	os.Setenv("AWS_SDK_LOAD_CONFIG", "1")
+
+	userConfig := viper.New()
+	userConfig.SetConfigName("app")
+	userConfig.AddConfigPath(".")
+	userConfig.AddConfigPath("config")
+	err := userConfig.ReadInConfig()
+	if err != nil {
+		return err
+	}
+	return nil
 }
